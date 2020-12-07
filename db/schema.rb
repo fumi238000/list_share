79794d1fe4dad51ce90f0c7d117d12416918c26f
fumi238000 <s21231128@gmail.com>
2020-12-07 18:49:20 +0900

    checkテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_094349) do
+ActiveRecord::Schema.define(version: 2020_12_07_094847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_12_07_094349) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "checks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_checks_on_task_id"
+    t.index ["user_id"], name: "index_checks_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -57,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_12_07_094349) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "checks", "tasks"
+  add_foreign_key "checks", "users"
   add_foreign_key "comments", "tasks"
   add_foreign_key "comments", "users"
   add_foreign_key "tasks", "categories"
