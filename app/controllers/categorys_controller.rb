000ab834a6c60dc6c0372234b621f97d8b45fb81
fumi238000 <s21231128@gmail.com>
2020-12-09 13:30:00 +0900

    カテゴリー一覧画面の作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,6 +1,23 @@
 class CategorysController < ApplicationController
   
   def index
+    @categorys = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    binding.pry
+    category = current_user.category.create!(category_params)
+    redirect_to category
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# テストユーザー
+EMAIL = "test@example.com"
+PASSWORD = "password"
+
+# データを全削除
+User.destroy_all
+
+#サンプルユーザー
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+
+user1.category.create!(name: "朝")
+user1.category.create!(name: "昼")
+user1.category.create!(name: "夜")
+user1.category.create!(name: "家事")
+
+
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: EMAIL, password: PASSWORD)
+
+
+puts "初期データの投入に成功しました！"
