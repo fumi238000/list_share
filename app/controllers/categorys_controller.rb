e25278c25cbccedfb45ac535ac84ee79a42d5051
fumi238000 <s21231128@gmail.com>
2020-12-09 15:53:01 +0900

    削除ボタンの作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,18 +1,21 @@
 class CategorysController < ApplicationController
-  before_action :set_category, only: %i[edit update destroy]
+  before_action :set_category, only: %i[show edit update destroy]
 
 
   def index
-    @categorys = Category.all
+    @categorys = Category.order(:id)
   end
+
 
   def new
     @category = Category.new
   end
 
+  def show
+  end
+
   def create
-    category = current_user.category.create!(category_params)
-    
+    category = current_user.category.create!(category_params)  
     if category.save
       redirect_to categorys_path, notice:"作成しました"
     else
@@ -21,29 +24,32 @@ class CategorysController < ApplicationController
     end
   end
 
+
   def edit    
   end
+
 
   def update
     @category.updae!(category_params)
     redirect_to @category
   end
 
+
   def destroy
     @category.destroy!
-    redirect_to category_path
+    redirvect_to @category
   end
+
 
   private
 
   def set_category
-    @category = current_user.categorys.find(params[:id])
-    redirect_to category_path, alert: "権限がありません"
+    category = current_user.categorys.find(params[:id])
+    # redirect_to category_path, alert: "権限がありません"
   end
 
 
   def category_params
-    params.require(:category).permit(:name)
-
+    params.require(:category).permit(:name ,:id)
   end
 end
