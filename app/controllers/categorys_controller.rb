8877f4cf7cbced48e03426b835f80bf82596c1f7
fumi238000 <s21231128@gmail.com>
2020-12-09 14:48:58 +0900

    destroy,editアクションの作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,5 +1,5 @@
 class CategorysController < ApplicationController
-  
+  before_action :set_category, only: %i[edit update destroy]
 
 
   def index
@@ -11,23 +11,39 @@ class CategorysController < ApplicationController
   end
 
   def create
-    @category = current_user.category.create!(category_params)
+    category = current_user.category.create!(category_params)
     
-    if @category.save
+    if category.save
       redirect_to categorys_path, notice:"作成しました"
     else
       flash.now[:alert] = "作成に失敗しました"
       render :new
     end
-    
-    
-    
   end
 
+  def edit    
+  end
+
+  def update
+    @category.updae!(category_params)
+    redirect_to @category
+  end
+
+  def destroy
+    @category.destroy!
+    redirect_to category_path
+  end
 
   private
 
+  def set_category
+    @category = current_user.categorys.find(params[:id])
+    redirect_to category_path, alert: "権限がありません"
+  end
+
+
   def category_params
     params.require(:category).permit(:name)
+
   end
 end
