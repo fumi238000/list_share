0f8993bc36e554b6921a4f2dac193e5710a74066
fumi238000 <s21231128@gmail.com>
2020-12-09 14:32:52 +0900

    new/createアクションの作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -11,8 +11,17 @@ class CategorysController < ApplicationController
   end
 
   def create
-    category = current_user.category.create!(category_params)
-    redirect_to categorys_path, notice:"作成しました"
+    @category = current_user.category.create!(category_params)
+    
+    if @category.save
+      redirect_to categorys_path, notice:"作成しました"
+    else
+      flash.now[:alert] = "作成に失敗しました"
+      render :new
+    end
+    
+    
+    
   end
 
 
