ae43351e3ce27ae09935e6f3c25750057a63b3ba
fumi238000 <s21231128@gmail.com>
2020-12-11 20:51:35 +0900

    taskテーブルのnameカラムに初期データを導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,21 @@ PASSWORD = "password"
 # データを全削除
 User.destroy_all
 
-#サンプルユーザー
 user1 = User.create!(name: "佐藤", email: "satou@example.com", password: "password")
 user2 = User.create!(name: "鈴木", email: "suzuki@example.com", password: "password")
 
-#これ絶対にリファクタリングできるよ
-user1.category.create!(name: "朝ルーティン")
-user1.category.create!(name: "昼ルーティン")
-user1.category.create!(name: "夜ルーティン")
-user1.category.create!(name: "家事")
+#同じカラムに複数のデータを入れたいので検討する
+category1 = user1.category.create!(name: "朝ルーティン")
+category1.task.create!(name: "水を飲む")
+category1.task.create!(name: "シャワーを浴びる")
+category1.task.create!(name: "瞑想する")
 
-user2.category.create!(name: "散歩ルーティン")
-user2.category.create!(name: "筋トレルーティン")
-user2.category.create!(name: "ナイトルーティン")
-user2.category.create!(name: "家事")
 
+category2 = user2.category.create!(name: "朝ルーティン")
+category2.task.create!(name: "水を飲む")
 
 
 # ログイン時に使用するアカウント（変数への代入は不要）
 User.create!(name: NAME, email: EMAIL, password: PASSWORD)
-
 
 puts "初期データの投入に成功しました！"
