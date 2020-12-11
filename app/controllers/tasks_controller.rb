09df3f09530814bef089455cf0551bc7b6992cb5
fumi238000 <s21231128@gmail.com>
2020-12-11 21:10:10 +0900

    タスク一覧画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
   
   def index
+    @tasks = Task.all
+    # @tasks = current_user.category.order(:id)
+    # @tasks = Task.all
+    # @categorys = current_user.category.order(:id)
   end
 
 end
