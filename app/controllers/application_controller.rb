89fc27df671fa1737d9f996c9c3a9dc03edcdc33
fumi238000 <s21231128@gmail.com>
2020-12-11 19:59:22 +0900

    ログインしていない場合、ログイン画面へ移動するように設定/リレーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :authenticate_user!
+
   # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource)
     user_path(resource.id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,18 @@ User.destroy_all
 #サンプルユーザー
 user1 = User.create!(name: "佐藤", email: "satou@example.com", password: "password")
 user2 = User.create!(name: "鈴木", email: "suzuki@example.com", password: "password")
-user3 = User.create!(name: "田中", email: "tanaka@example.com", password: "password")
 
-
-user1.category.create!(name: "朝")
-user1.category.create!(name: "昼")
-user1.category.create!(name: "夜")
+#これ絶対にリファクタリングできるよ
+user1.category.create!(name: "朝ルーティン")
+user1.category.create!(name: "昼ルーティン")
+user1.category.create!(name: "夜ルーティン")
 user1.category.create!(name: "家事")
+
+user2.category.create!(name: "散歩ルーティン")
+user2.category.create!(name: "筋トレルーティン")
+user2.category.create!(name: "ナイトルーティン")
+user2.category.create!(name: "家事")
+
 
 
 # ログイン時に使用するアカウント（変数への代入は不要）
