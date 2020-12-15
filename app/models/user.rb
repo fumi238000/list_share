a54cef11b0fefbe1f5e87b3e38167e530a743482
fumi238000 <s21231128@gmail.com>
2020-12-15 14:27:28 +0900

    usersテーブルのimageカラムを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-    def self.guest
-      find_or_create_by!(name: "ゲストユーザー", email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      end
+         
+  mount_uploader :image, ImageUploader
+    
+  def self.guest
+    find_or_create_by!(name: "ゲストユーザー", email: 'guest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
+  end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_095223) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.string "image", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
