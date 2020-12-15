7a63cd1234ce448e058459b5f9bdb3fd1c97e290
fumi238000 <s21231128@gmail.com>
2020-12-15 14:28:42 +0900

    写真を保存するPostテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_095223) do
+ActiveRecord::Schema.define(version: 2020_12_15_052809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 2020_12_07_095223) do
     t.string "owner_id", null: false
     t.string "participation_id", null: false
     t.string "category", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
