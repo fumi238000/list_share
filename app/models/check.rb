96844e3cd2a48eea88c88c1e2948592e2257e5a0
fumi238000 <s21231128@gmail.com>
2020-12-18 00:11:05 +0900

    リレーションの調整

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,4 +1,4 @@
 class Check < ApplicationRecord
   belongs_to :user
-  belongs_to :task
+  # belongs_to :task
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,6 @@ class Task < ApplicationRecord
 
   # has_many :comment, dependent: :destroy
 
-  has_many :check, dependent: :destroy, foreign_key: 'task_id'
+  # has_many :check, dependent: :destroy, foreign_key: 'task_id'
   belongs_to :category
 end
