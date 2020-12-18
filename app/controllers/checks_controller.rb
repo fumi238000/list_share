c45fafcfb60478428c34e93eb6f2951f46f71bef
fumi238000 <s21231128@gmail.com>
2020-12-18 12:29:05 +0900

    checkボックスを実装

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,13 +1,16 @@
 class ChecksController < ApplicationController
 
   def create
-    Check.create!(task_id: params[:task_id],user_id:current_user[:id])
-    redirect_back(fallback_location: root_path)
+    @task = Task.find(params[:task_id])
+    Check.create!(task_id: @task.id,user_id: current_user[:id])
+    redirect_to root_path
   end
 
   def destroy
-    Check.find_by(task_id: params[:task_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @task = Task.find(params[:task_id])
+    Check.find_by(task_id: @task.id).destroy!
+    redirect_to root_path
+  
   end
 
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -2,10 +2,10 @@ class Check < ApplicationRecord
   belongs_to :user
   # belongs_to :task
 
-  validates :user_id, uniqueness: {
-    scope: :task_id,
-    message: "チェックできません。これいる？"
-  } 
+  # validates :user_id, uniqueness: {
+  #   scope: :task_id,
+  #   message: "チェックできません。"
+  # } 
 
 
 end
