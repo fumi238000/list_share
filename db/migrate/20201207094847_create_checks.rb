e842f4b5566139d8b87bdd4750fd013b8ba73d16
fumi238000 <s21231128@gmail.com>
2020-12-18 22:51:54 +0900

    checkテーブルのデータ型を調整

--- db/migrate/20201207094847_create_checks.rb
+++ db/migrate/20201207094847_create_checks.rb
@@ -2,7 +2,7 @@ class CreateChecks < ActiveRecord::Migration[6.0]
   def change
     create_table :checks do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :task_id, null: false
+      t.integer :task_id, null: false
 
       t.timestamps
       # add_index :checks, [:user_id, :task_id], unique: true
