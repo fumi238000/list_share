d494594fe09aec9925d2d73414a33d7693073506
fumi238000 <s21231128@gmail.com>
2020-12-20 13:59:33 +0900

    調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :category_id, presence: true, length: { maximum: 30 }
-
   # has_many :comment, dependent: :destroy
-
   # has_many :check, dependent: :destroy, foreign_key: 'task_id'
   belongs_to :category
 
@@ -11,14 +9,5 @@ class Task < ApplicationRecord
   def checked_by?(t)
     Check.find_by(task_id: t.id).present?
   end
-
-
-  #カテゴリー作成判定メソッド
-  def current_user_create_category?
-    binding.pry
-    @category = Category.all
-    @category = @category.where(user_id: current_user[:id]).present?
-  end
-
 
 end
