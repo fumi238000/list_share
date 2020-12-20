34872382248b42810411f5bfafdf93e99f5b1a38
fumi238000 <s21231128@gmail.com>
2020-12-20 17:58:44 +0900

    ゲストログインの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,12 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader       
 
 
+
   def self.guest
-    find_or_create_by!(name: "ゲストユーザー", email: 'guest@example.com', image: 'default.jpeg') do |user|
+    find_or_create_by!(name: "ゲストユーザー", email: 'guest@example.com') do |user|
     user.password = SecureRandom.urlsafe_base64
     # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
-
-
 
 end
