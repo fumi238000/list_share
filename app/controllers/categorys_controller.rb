08c05e0cebf25bc15663b375d38881aa96867c59
fumi238000 <s21231128@gmail.com>
2020-12-20 12:10:54 +0900

    不要なコードの削除

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -27,17 +27,14 @@ class CategorysController < ApplicationController
 
 
   def update
-    if @category.update!(category_params)
-      redirect_to categorys_path(current_user), notice: "更新しました"
-    else
-      redirect_to categorys_path(current_user), alert: "あなたには権限がありません"
-    end
+    @category.update!(category_params)
+    redirect_to categorys_path, notice: "更新しました"
   end
 
 
   def destroy
     @category.destroy!
-    redirect_to categorys_path(current_user), alert: "削除しました"
+    redirect_to categorys_path, alert: "削除しました"
   end
 
 
@@ -47,25 +44,17 @@ class CategorysController < ApplicationController
     params.require(:category).permit(:name)
   end
 
+
+
   def set_category
-    # カテゴリーに含まれるuser_idとログインユーザーが一致しているか？
+    # カテゴリーに含まれるuser_idとログインユーザーが一致しているか
     @category = Category.find(params[:id])
 
     if @category.user_id === current_user[:id]
-      #一致
       @category = current_user.category.find(params[:id])
-    
     else
-
-      #不一致
       redirect_to categorys_path, alert: "権限がありません"
     end
   end
-
-
-# def set_category
-#   @category = current_user.category.find(params[:id])
-# end
-
 end
  
