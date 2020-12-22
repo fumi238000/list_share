c22a8a56eb15cee9c102e961aa61d75989ddfc02
fumi238000 <s21231128@gmail.com>
2020-12-22 22:43:01 +0900

    調整

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,14 +3,13 @@ class ChecksController < ApplicationController
   def create
     @task = Task.find(params[:task_id])
     Check.create!(task_id: @task.id,user_id: current_user[:id])
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @task = Task.find(params[:task_id])
     Check.find_by(task_id: @task.id).destroy!
-    redirect_to root_path
-  
+    redirect_back(fallback_location: root_path)
   end
 
 end
