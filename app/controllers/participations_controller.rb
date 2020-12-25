cb204391b317fe6b385b4ae3750b6475011b0842
fumi238000 <s21231128@gmail.com>
2020-12-25 18:26:46 +0900

    バリデージョンの設定

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,6 +2,7 @@ class ParticipationsController < ApplicationController
   before_action :login_check
   before_action :participation_params, only: %i[create]
   before_action :set_participation, only: %i[destroy]
+  before_action :set_ransack, only: %i[new]
 
 
   PER_PAGE = 3
@@ -9,8 +10,6 @@ class ParticipationsController < ApplicationController
   def new
     @participation = Participation.new
     @category = params[:category]
-    @q = User.ransack(params[:q])
-    @search_users= @q.result(distinct: true).limit(PER_PAGE)
   end
 
 
@@ -31,7 +30,7 @@ class ParticipationsController < ApplicationController
       redirect_to participation_path(participation_params[:category_id]), notice:"作成しました"
     
     else
-      redirect_to new_participation_path, alert: "全て入力してください"
+      redirect_to new_participation_path, alert: "入力に誤りがあります。すでに登録されている可能性もあります。"
     end
   end
 
@@ -51,6 +50,11 @@ class ParticipationsController < ApplicationController
     @participation = Participation.find(params[:id])
     # cuurent_userのみ消せないように後から設定する
     # redirect_to category_path, alert: "権限がありません"
+  end
+
+  def set_ransack
+    @q = User.ransack(params[:q])
+    @search_users= @q.result(distinct: true).limit(PER_PAGE)
   end
 
 end
