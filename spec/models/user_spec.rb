05fc5632415902911542b35619557a9b0aafd481
fumi238000 <s21231128@gmail.com>
2021-01-02 23:54:42 +0900

    userのモデルスペックを実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,77 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "データが条件を満たす時" do
-    it "保存ができる" do
-      user = build (:user)
-      expect(user.valid?).to eq true
-    end
-  end
-
 
   describe "バリデーション" do
-    
+    subject {user.valid?} 
+
+    context "データが条件を満たす時" do
+      let(:user) { build(:user) }
+      it "保存ができる" do
+        expect(subject).to eq true
+      end
+    end
+  
     context "名前が空のとき" do
+      let(:user) { build(:user, name: "") }
       it "エラーが発生する" do
-        user = build(:user,name: "")
-        expect(user.valid?).to eq false
+        expect(subject).to eq false
         expect(user.errors.messages[:name]).to include "を入力してください"
       end
     end
 
     context "emailが空のとき" do
+      let(:user) { build(:user, email: "") }
       it "エラーが発生する" do
-          user = build(:user,email: "")
-          expect(user.valid?).to eq false
+          expect(subject).to eq false
           expect(user.errors.messages[:email]).to include "を入力してください"
       end
     end
 
     context "passwordが空のとき" do
+      let(:user) { build(:user, password: "") }
       it "エラーが発生する" do
-        user = build(:user,password: "")
-        expect(user.valid?).to eq false
+        expect(subject).to eq false
         expect(user.errors.messages[:password]).to include "を入力してください"
       end
     end
 
-    context "name が20文字以上である時" do 
+    context "name が21文字以上である時" do 
+      let(:user)  { build(:user,name: "a" * 21) } 
       it "エラーが発生する" do
-        user = build(:user,name: "hogehogehogehogehogehoge")
-        expect(user.valid?).to eq false
+        expect(subject).to eq false
         expect(user.errors.messages[:name]).to include "は20文字以内で入力してください"
       end
     end
 
-
-    #emailがすでに存在する時は無効である
-    it "emailがすでに存在している時は無効であること" do
-      user = User.create(name: "fumiya", email: "email@email.com", password: "password")
-      user = User.new(name: "tomoya", email: "email@email.com", password: "password")
-      user.valid?
-      expect(user.errors[:email]).to include("はすでに存在します")
+    context "emailがすでに存在する時は無効である" do 
+      before { create(:user, email: "test@email.com") }
+      let (:user) { build(:user, email: "test@email.com")}
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(user.errors.messages[:email]).to include "はすでに存在します"
+      end
     end
-
-
-
-
-
-    # context "name が空のとき" do
-    #   let(:user) { build(:user, name: "") }
-    #   it "エラーが発生する" do
-    #     expect(subject).to eq false
-    #     expect(user.errors.messages[:name]).to include "を入力してください"
-    #   end
-    # end
-
-  #   context "email が空のとき" do
-  #     it "エラーが発生する" do
-  #     end
-  #   end
-  # end
-
+    
+    #未実装
+    context "ユーザーが削除されたとき" do
+      subject { user.destroy }
   
+      let(:user) { create(:user) }
+      before do
+        create_list(:message, 2, user: user)
+        create(:message)
+      end
+    end
   end
-
 
 end
