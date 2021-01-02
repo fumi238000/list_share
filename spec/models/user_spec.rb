54cde030517f18da87560a78fb178bc2dbb16873
fumi238000 <s21231128@gmail.com>
2021-01-02 21:52:34 +0900

    Userのバリデージョンテストの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context "データが条件を満たす時" do
+    it "保存ができる" do
+      user = build (:user)
+      expect(user.valid?).to eq true
+    end
+  end
+
+
   describe "バリデーション" do
-    #名前がなければ無効な状態である
-    it "名前がなければ無効な状態であること" do
-      user = User.new(name: "", email: "email@email.com", password: "12345678")
-      user.valid?
-      expect(user.errors[:name]).to_not include("can't be blacnk")
-    end
     
-    #emailがなければ無効な状態である
-    it "emailがなければ無効な状態であること" do
-      user = User.new(name: "fumiya", email: "", password: "12345678")
-      user.valid?
-      expect(user.errors[:name]).to_not include("can't be blacnk")
+    context "名前が空のとき" do
+      it "エラーが発生する" do
+        user = build(:user,name: "")
+        expect(user.valid?).to eq false
+        expect(user.errors.messages[:name]).to include "を入力してください"
+      end
     end
 
-    #passwordがなければ無効な状態である
-    it "emailがなければ無効な状態であること" do
-      user = User.new(name: "fumiya", email: "email@email.com", password: "")
-      user.valid?
-      expect(user.errors[:name]).to_not include("can't be blacnk")
+    context "emailが空のとき" do
+      it "エラーが発生する" do
+          user = build(:user,email: "")
+          expect(user.valid?).to eq false
+          expect(user.errors.messages[:email]).to include "を入力してください"
+      end
+    end
+
+    context "passwordが空のとき" do
+      it "エラーが発生する" do
+        user = build(:user,password: "")
+        expect(user.valid?).to eq false
+        expect(user.errors.messages[:password]).to include "を入力してください"
+      end
+    end
+
+    context "name が20文字以上である時" do 
+      it "エラーが発生する" do
+        user = build(:user,name: "hogehogehogehogehogehoge")
+        expect(user.valid?).to eq false
+        expect(user.errors.messages[:name]).to include "は20文字以内で入力してください"
+      end
     end
 
 
+    #emailがすでに存在する時は無効である
+    it "emailがすでに存在している時は無効であること" do
+      user = User.create(name: "fumiya", email: "email@email.com", password: "password")
+      user = User.new(name: "tomoya", email: "email@email.com", password: "password")
+      user.valid?
+      expect(user.errors[:email]).to include("はすでに存在します")
+    end
 
 
 
