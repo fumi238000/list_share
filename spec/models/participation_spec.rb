18da59e8414638efa825286fa9f2ed2e89be9ae1
fumi238000 <s21231128@gmail.com>
2021-01-04 14:16:09 +0900

    [add]participation_spec.rbにテストを追加
    "participation_idがinteger以外の時/エラーが発生する"テストを実装

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Participation, type: :model do
     end
 
     context "participation_idがintegerの時" do
-    let(:participation) { create(:participation, participation_id: "1") }  
+    let(:participation) { build(:participation, participation_id: "1") }  
       it "保存できる" do
         expect(subject).to eq true
       end
@@ -41,7 +41,15 @@ RSpec.describe Participation, type: :model do
         expect(participation.errors.messages[:category]).to include "を入力してください"
       end
     end
-
+    
+    context "participation_idがinteger以外の時" do
+      let(:participation) { build(:participation, participation_id: "hogehoge") }  
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(participation.errors.messages[:participation_id]).to include "は数値で入力してください"  
+      end
+    end
+    
     # context "owner_idが存在しないUserの場合" do
     #   it "エラーが発生する" do
     #   end
@@ -51,12 +59,6 @@ RSpec.describe Participation, type: :model do
     #   it "エラーが発生する" do
     #   end
     # end
-
-    # context "participation_idがinteger以外の時" do
-    #   it "エラーが発生する" do
-    #   end
-    # end
-
-
+    
   end
 end
