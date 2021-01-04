de099f9d7f6c1194b17a6843b3ac1ca02ba9a0c9
fumi238000 <s21231128@gmail.com>
2021-01-04 12:17:10 +0900

    [clean]リファクタリング

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -40,46 +40,11 @@ RSpec.describe Task, type: :model do
         expect(subject).to eq false
         expect(task.errors.messages[:name]).to include "は30文字以内で入力してください"
       end
-
     end
-
-
-  
+    
   end
-
 end
-  #   context "nameが20文字以下の場合"   do
-  #     let(:category) { build(:category, name: "a" * 3) }
-  #     it "保存ができる" do
-  #       expect(subject).to eq true
-  #     end
-  #   end
-
-  #   context "ユーザーが存在しない時" do
-  #     let(:category) { build(:category, user_id: "") }
-  #     it "エラーが発生する" do
-  #       expect(subject).to eq false
-  #       expect(category.errors.messages[:user]).to include "を入力してください"
-  #     end
-  #   end
-
-  #   context "nameが空の時" do
-  #     let(:category) { build(:category, name: "") }
-  #     it "エラーが発生する" do
-  #       expect(subject).to eq false
-  #       expect(category.errors.messages[:name]).to include "を入力してください"
-  #     end
-  #   end
-
-  #   context "nameが21文字以上の場合" do
-  #     let(:category) { build(:category, name: "a" * 21) }
-  #     it "エラーが発生する" do
-  #       expect(subject).to eq false
-  #       expect(category.errors.messages[:name]).to include "は20文字以内で入力してください"
-  #     end
-  #   end
   
-  # end
 
     # describe "scope" , type: :doing do
     #   context "データを保存した時" do
