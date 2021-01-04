f6b2915b5ee34e93c3ccfc97e0a86d99a5f0be5c
fumi238000 <s21231128@gmail.com>
2021-01-04 11:30:39 +0900

    [clean]リファクタリング
    category_spec.rbのリファクタリング

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,47 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe "バリデーション" do
-      subject {category.valid?} 
+    subject {category.valid?} 
    
-      context "データが条件を満たす時" do
-         let(:category) { build(:category) }
-           it "保存ができる" do
-           expect(subject).to eq true
-        end
-      end
-
-      context "nameが20文字以下の場合" , type: :doing  do
-        let(:category) { build(:category, name: "a" * 3) }
-          it "保存ができる" do
-          expect(subject).to eq true
-        end
-      end
-
-
-      context "ユーザーが存在しない時" do
-        let(:category) { build(:category, user_id: "") }
-          it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(category.errors.messages[:user]).to include "を入力してください"
-        end
-      end
-
-      context "nameが空の時" do
-        let(:category) { build(:category, name: "") }
-          it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(category.errors.messages[:name]).to include "を入力してください"
-        end
-      end
-
-      context "nameが21文字以上の場合" do
-        let(:category) { build(:category, name: "a" * 21) }
-          it "エラーが発生する" do
-          expect(subject).to eq false
-          expect(category.errors.messages[:name]).to include "は20文字以内で入力してください"
-        end
+    context "データが条件を満たす時" do
+      let(:category) { build(:category) }
+      it "保存ができる" do
+        expect(subject).to eq true
       end
     end
+
+    context "nameが20文字以下の場合" , type: :doing  do
+      let(:category) { build(:category, name: "a" * 3) }
+      it "保存ができる" do
+        expect(subject).to eq true
+      end
+    end
+
+
+    context "ユーザーが存在しない時" do
+      let(:category) { build(:category, user_id: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(category.errors.messages[:user]).to include "を入力してください"
+      end
+    end
+
+    context "nameが空の時" do
+      let(:category) { build(:category, name: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(category.errors.messages[:name]).to include "を入力してください"
+      end
+    end
+
+    context "nameが21文字以上の場合" do
+      let(:category) { build(:category, name: "a" * 21) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(category.errors.messages[:name]).to include "は20文字以内で入力してください"
+      end
+    end
+  
+  end
 
     # describe "scope" , type: :doing do
     #   context "データを保存した時" do
