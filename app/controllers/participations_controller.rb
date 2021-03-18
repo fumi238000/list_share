0a66ae88a01c368db344074b86de4f6fd57f0e18
fumi238000 <s21231128@gmail.com>
2021-03-19 00:26:11 +0900

    [add]before_actionを実装

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,6 +1,7 @@
 class ParticipationsController < ApplicationController
   before_action :login_check
   before_action :set_participation, only: %i[destroy]
+  before_action :present_participation_user?, only: %i[create]
   # before_action :set_ransack, only: %i[new]
   # 登録しようとしているユーザーがログインユーザーではないか？
   # before_action :current_user?, only: %i[create]
@@ -13,28 +14,21 @@ class ParticipationsController < ApplicationController
     @category_id = params[:format]
   end
 
-  # リファクタリングすること
   def show
+    # TODO: N+１問題を解消すること
     @participations = Participation.where(owner_id: current_user.id)
-    # @participations = Participation.includes(owner_id: current_user.id)
     @category_id = params[:id]
   end
 
   def create
-    # ログインユーザーの登録を阻止
-    # TODO: before_actionにて実装する
-    redirect_to participation_path(participation_params[:owner_id]), alert: 'ログインユーザーは登録できません' and return if participation_params[:user_id].to_i == current_user.id
-    
-    #同じユーザーの登録を阻止 
-    redirect_to participation_path(participation_params[:owner_id]), alert: 'すでに登録されたユーザーです' and return if Participation.exists?(user_id: participation_params[:user_id])
+    present_participation_user?
 
     @participation = Participation.new(participation_params)
-    
     if @participation.save
       redirect_to participation_path(@participation[:owner_id]), notice: '共有者を追加しました'
     else
       @category_id = participation_params[:category_id]
-      render :new and return
+      render :new
       # @owner_id = current_user[:id]
       # @q = User.ransack(params[:q])
       # @search_users = @q.result(distinct: true).limit(PER_PAGE)
@@ -54,8 +48,6 @@ class ParticipationsController < ApplicationController
 
   def set_participation
     @participation = Participation.find(params[:id])
-    # cuurent_userのみ消せないように後から設定する
-    # redirect_to category_path, alert: "権限がありません"
   end
 
   # def set_ransack
@@ -63,13 +55,10 @@ class ParticipationsController < ApplicationController
   #   @search_users = @q.result(distinct: true).limit(PER_PAGE)
   # end
 
-  # def current_user?
-  #   binding.pry
-  #   if current_user[:id] == participation_params[:user_id].to_i
-  #     render :new, alert: '自分自身は登録できません'
-  #     # redirect_to new_participation_path, alert: '自分自身は登録できません'
-  #   end
-  # end
-
-
+  def present_participation_user?
+    # ログインユーザーのチェック
+    redirect_to participation_path(participation_params[:owner_id]), alert: 'ログインユーザーは登録できません' and return if participation_params[:user_id].to_i == current_user.id
+    # 同じユーザーの登録を阻止 
+    redirect_to participation_path(participation_params[:owner_id]), alert: 'すでに登録されたユーザーです' and return if Participation.exists?(user_id: participation_params[:user_id])
+  end
 end
